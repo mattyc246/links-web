3b09405e7b179d381d097147ee0c606f7bd74881
Matthew Cross <mattyc246@gmail.com>
2020-07-16 19:49:42 +0800

    layout for products pages started

---
+++ src/pages/products.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Section from "../components/section"
+
+const Products = () => {
+  return (
+    <Layout>
+      <SEO title="products" />
+      <Section light="true">
+
+      </Section>
+      <Section>
+
+      </Section>
+      <Section light="true">
+
+      </Section>
+      <Section>
+
+      </Section>
+    </Layout>
+  )
+}
+
+export default Products

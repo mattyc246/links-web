2a29ce94c6958cc5618d743feda742c85fb71b0e
Matthew Cross <mattyc246@gmail.com>
2020-07-16 18:07:49 +0800

    homepage complete minus carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,12 @@ import React from "react"
 import styled from "styled-components"
 import Mustang from "../images/mustang.jpg"
 import Mustang2 from "../images/mustang-2.jpg"
+import Brake from "../images/automobile.png"
+import Oil from "../images/motor.png"
+import Piston from "../images/piston.png"
+import Wrench from "../images/repair-wrenches.png"
+import Speedo from "../images/speed.png"
+import Suspension from "../images/suspension.png"
 
 import Section from "../components/section"
 import Layout from "../components/layout"
@@ -51,7 +57,7 @@ const IndexPage = () => (
       <Container>
         <Row className="align-items-center">
           <Col lg={6}>
-            <h3 className="text-center text-lg-left my-4">Our History</h3>
+            <h2 className="text-center text-lg-left my-4">Our History</h2>
             <p className="text-justify">
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
               nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
@@ -65,8 +71,68 @@ const IndexPage = () => (
               dolor sit amet.
             </p>
           </Col>
-          <Col lg={{span: 5, offset: 1}}>
+          <Col lg={{ span: 5, offset: 1 }}>
             <img className="w-100" src={Mustang2} alt="mustang" />
+          </Col>
+        </Row>
+      </Container>
+    </Section>
+    <Section></Section>
+    <Section light="true">
+      <Container>
+        <Row className="align-items-center">
+          <Col lg={6}>
+            <Row>
+              <Col className="my-3" xs={6} lg={4}>
+                <img
+                  className="w-75 mx-auto d-block"
+                  src={Wrench}
+                  alt="Wrench"
+                />
+              </Col>
+              <Col className="my-3" xs={6} lg={4}>
+                <img
+                  className="w-75 mx-auto d-block"
+                  src={Speedo}
+                  alt="Speedometer"
+                />
+              </Col>
+              <Col className="my-3" xs={6} lg={4}>
+                <img
+                  className="w-75 mx-auto d-block"
+                  src={Piston}
+                  alt="Piston"
+                />
+              </Col>
+              <Col className="my-3" xs={6} lg={4}>
+                <img
+                  className="w-75 mx-auto d-block"
+                  src={Suspension}
+                  alt="Suspension"
+                />
+              </Col>
+              <Col className="my-3" xs={6} lg={4}>
+                <img className="w-75 mx-auto d-block" src={Brake} alt="Brake" />
+              </Col>
+              <Col className="my-3" xs={6} lg={4}>
+                <img className="w-75 mx-auto d-block" src={Oil} alt="Oil" />
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={{ span: 5, offset: 1 }}>
+            <h2 className="text-center text-lg-left my-4">Our Services</h2>
+            <p className="text-justify">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+              amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+              invidunt ut labore et dolore magna aliquyam erat, sed diam
+              voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+              dolor sit amet.
+            </p>
           </Col>
         </Row>
       </Container>

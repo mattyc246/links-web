59862dfe29ce35ed397711762f7aac0ccb53c188
Matthew Cross <mattyc246@gmail.com>
2020-08-13 15:39:33 +0800

    added content component

---
+++ src/components/content.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const BodyContent = styled.div`
+  h1,h2,h3,h4,h5,h6 {
+    font-weight: 700;
+  }
+
+  p {
+    text-align: justify;
+  }
+`
+
+const Content = ({children}) => {
+  return (
+    <BodyContent>
+      {children}
+    </BodyContent>
+  )
+}
+
+export default Content
